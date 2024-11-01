a5a3b5fbe82abf613945761395263fcb37e62c2b
dat <goodboy30112003@gmail.com>
2024-11-01 21:57:01 +0700

    add metadata for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,29 @@ import App from "@/componemt/homePage/App";
 import { fetchPokemonById, fetchPokemonListByLink } from "@/services/Pokemon";
 import { handleRandom } from "@/utils/RadomNumber";
 import "../css/home.css";
-
+export async function generateMetadata() {
+  try {
+    return {
+      title: " Pokémons",
+      description: "Get more information about the pokemon you like.",
+      openGraph: {
+        title: "Pokémons",
+        description: "Get more information about the pokemon you like.",
+        images: [
+          {
+            url: "/random_center_bg.webp",
+            width: 800,
+            height: 800,
+            alt: "pokemon",
+          },
+        ],
+      },
+    };
+  } catch (error) {
+    console.error("Error generating metadata:", error);
+    return {};
+  }
+}
 export default async function HomePage() {
   // Fetch the initial list of Pokémon
   const { results, next } = await fetchPokemonListByLink(
@@ -15,7 +37,7 @@ export default async function HomePage() {
     radomnumber: [],
   });
 
-  const requests = randomIds.map((id) => fetchPokemonById(`${id}`));
+  const requests = randomIds.map((id) => fetchPokemonById("${id}"));
   const randomResponses = await Promise.all(requests);
 
   return <App results={results} next={next} random={randomResponses} />;
