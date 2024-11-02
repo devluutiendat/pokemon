ebacba2c95db7c6cf4d4bb51f79414b8f6200061
dat <goodboy30112003@gmail.com>
2024-11-02 14:45:03 +0700

    add pathname for config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
         remotePatterns:[
             {
                 protocol:"https",
-                hostname:"raw.githubusercontent.com"
+                hostname:"raw.githubusercontent.com",
+                pathname:"/**"
             }
         ]
     }
